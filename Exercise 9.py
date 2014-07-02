39acb2583976059de63a988d417088b6ebb4fde4
Noe Rodriguez <n@n.com>
2014-07-01 19:38:35 -0600

    add step two to project nine

--- Exercise 9.py
+++ Exercise 9.py
@@ -12,20 +12,44 @@ print the number of iterations (we need to create a code to count the iterations
 
 *2 - answer from the loop against error."""
 
-error = 0.00001
-iterations = 0
+error = 0.000001
 
-x = input("Please enter a number to calculate to the square root of :")
-a = input("Please enter another number as starting point:")
-next_x = x ** .5
-a = float(x +(a / x)) / 2.0 - next_x
-diff = a - error
-print diff
+#a = input("Please enter a number to calculate to the square root of :")
+#old_x = input("Please enter another number as starting point:")
+
+def newton_function(x, a):
+    iterations = 0
+    next_x = (x +(a / x)) / 2.0
+    diff = abs(x - next_x)
+
+    while diff > error:
+
+        x = next_x
+        next_x =( x +(a / x)) / 2
+        diff = (x - next_x)
+        iterations += 1
+
+    return next_x
+
+#print "The square root is ", new_x
 
 
+import sys
+import math
+
+def test():
+    sys.stdout.write("{:<3}{:<20}{:<20}{:<20}{:<20}\n".format('n', "square_root", "math.sqrt", "difference", "is difference < 0.000001?"))
+    for x in range(1, 10):
+        current_num = str(x)
+        my_sqrt = newton_function(5, x)
+        py_sqrt = math.sqrt(x)
+        diff = abs(my_sqrt - py_sqrt)
+        is_good = diff < 0.000001
+        sys.stdout.write("{:<3}{:<20}{:<20}{:<20}{:<20}\n".format(current_num, str(my_sqrt), str(py_sqrt), str(diff), str(is_good)))
+
+test()
+#print newton_function(old_x, a)
 
 
-
-sh
 
 
